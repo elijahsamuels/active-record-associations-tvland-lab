24c1852e5e6694bfa23d55dfd201bf3f75fea956
elijahsamuels <elijahsamuels@gmail.com>
2020-12-16 16:43:24 -0700

    commit tree d2c4a19ff92ea909627c1ee1e2394d4fd19448fc

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+  has_many :actors, through: :characters
+
+  # get full names of actors from shows
+    def actors_list
+        self.actors.collect { |a| a.full_name}    
+    end
+
 end
